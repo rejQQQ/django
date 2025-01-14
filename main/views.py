7b3853498b7bdff8852ea4bcad707cd561d17be1
rejQ <rejqqq@gmail.com>
2025-01-14 15:58:12 +0200

    main app

--- main/views.py
+++ main/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+def index(request):
+    return HttpResponse("<h4>АЗХзхазхазхзах</h4>")
+
+def about(request):
+    return HttpResponse("<h4>about</h4>")
