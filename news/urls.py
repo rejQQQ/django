25cbbd39fe7f18f19d47c17c8a7c10e4e65b26ed
rejQ <rejqqq@gmail.com>
2025-01-17 17:18:16 +0200

    урок 10, динамические страницы

--- news/urls.py
+++ news/urls.py
@@ -1,9 +1,8 @@
-
 from . import views
 from django.urls import path
 
 urlpatterns = [
     path('', views.news_home, name='news_home'),
     path('create', views.create, name='create'),
-
+    path('<int:pk>/', views.NewsDetailView.as_view(), name='news-detail')  # Исправлено имя на 'news-detail'
 ]
